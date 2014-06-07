1795825d7cc79e417e41f2cc159317bb9d2eed78
Tai Phuoc Tran <tait@tp.(none)>
2014-06-07 12:24:29 -0600

    initial commit

---
+++ src/main/java/com/sun/tools/example/debug/App.java
@@ -0,0 +1,13 @@
+package com.sun.tools.example.debug;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
